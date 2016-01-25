a2cdeb209ae9276321e62e7638a881908698a6b6
Erik Woitschig <erik@sourcloud.com>
2016-01-25 21:46:10 +0100

    simple filter added, start/stop fixed, leading zero fix

--- app/view/winamp/slider/Vslider.js
+++ app/view/winamp/slider/Vslider.js
@@ -6,10 +6,10 @@ Ext.define('Playground.view.winamp.slider.Vslider', {
     'Ext.slider.Single'
   ],
 
-  value: 50,
-  increment: 10,
+  value: 100,
+  increment: 100,
   minValue: 0,
-  maxValue: 100,
+  maxValue: 5000,
   vertical: true,
-  height: 100  
+  height: 100
 });
